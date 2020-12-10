9a7648964b272b542e239af8859b522e4fe845a0
Meryl Rastall <merylrastall@gmail.com>
2020-12-10 12:00:08 +0000

    Created first test and passed

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe ("register testing", () => {
+  test("render a register button", () => {
+
+    const { getByText } = render(<App />);
+    const linkElement = getByText("Register");
+    expect(linkElement).toBeInTheDocument();
+  })
+})
+
