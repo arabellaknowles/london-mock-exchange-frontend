fd142aa2964de56e685746eef106b31df09cffaa
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2021-01-14 17:04:20 +0000

    Loaded portfolio form from Dashboard
    - newfeed button renders newsfeed from all pages
    - home button renders home from portfolio form now
    - removed portfolio form rendering from portfolio list

--- src/components/Portfolio/PortfolioList.js
+++ src/components/Portfolio/PortfolioList.js
@@ -1,35 +1,20 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Portfolio from './Portfolio'
-import PortfolioForm from './PortfolioForm'
+
 
 export default class PortfolioList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       portfolios: [],
-      loadingPortfolioForm: false
     };
     
-    this.loadPortfolioForm = this.loadPortfolioForm.bind(this)
-    this.notShowPortfolioForm = this.notShowPortfolioForm.bind(this)
     this.loadPortfolios = this.loadPortfolios.bind(this)
   }
 
   componentDidMount() {
     this.loadPortfolios()
-  }
-
-  notShowPortfolioForm(){
-    this.setState({
-      loadingPortfolioForm: false
-    })
-  }
-
-  loadPortfolioForm(){
-    this.setState({
-      loadingPortfolioForm: true 
-    })
   }
 
   loadPortfolios() {
@@ -48,26 +33,16 @@ export default class PortfolioList extends Component {
   }
 
   render() {
-    let portfolios = this.state.portfolios
-    if(this.state.loadingPortfolioForm){
-      return (
-        <div class="container">
-            <PortfolioForm loadPortfolios={this.loadPortfolios} userToken={this.props.userToken} notShowPortfolioForm={this.notShowPortfolioForm} /> 
-        </div>
-      )
-    } else {
-      return(
-        <div class="container">
-          <button class="btn btn-success" onClick={this.loadPortfolioForm}>Create New Portfolio</button>
-          <table class="table table-bordered" >
-            <tbody>
-              {portfolios.map((portfolio) => 
-                <Portfolio name={portfolio.name} net_earnings={portfolio.net_earnings} userToken={this.props.userToken} id={portfolio.id} loadPortfolio={this.props.loadPortfolio}/>
-              )}
-            </tbody>
-          </table>
-        </div>
-      )
-    }
+    return (
+      <div class="container">
+        <table class="table table-bordered" >
+          <tbody>
+            {this.state.portfolios.map((portfolio) => 
+              <Portfolio name={portfolio.name} net_earnings={portfolio.net_earnings} userToken={this.props.userToken} id={portfolio.id} loadPortfolio={this.props.loadPortfolio}/>
+            )}
+          </tbody>
+        </table>
+      </div>
+    )
   }
 }
