454278508cc9c2f660ef2e54bce751f7a3a40972
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2021-01-14 17:56:36 +0000

    Home and newsfeed buttons only rendeered once signed in

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,20 +13,20 @@ export default class Header extends Component {
     return (
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
       <div class="container">
-        <a class="navbar-brand">London Mock Exchange</a>
+        <p class="navbar-brand">London Mock Exchange</p>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
+          { this.props.userToken !== null ? <li class="nav-item active">
               <button type="button" class="btn btn-dark" onClick={this.props.loadDashboard}>Home
                 <span class="sr-only">(current)</span>
                 </button>
-            </li>
-            <li class="nav-item">
+            </li> : '' }
+            { this.props.userToken !== null ? <li class="nav-item">
               <button type="button" class="btn btn-dark" onClick={this.props.loadNewsList}>Newsfeed</button>
-            </li>
+            </li> : '' }
             { this.props.userToken !== null ? <li class="nav-item">
               <button type="button" class="btn btn-dark" onClick={this.props.handleLogout}>Logout</button>
             </li> : '' }
